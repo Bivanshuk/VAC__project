1795d4b491adbdecd2d4e667c926f5a8c0d9d828
Bivanshuk <108558814+Bivanshuk@users.noreply.github.com>
2024-03-14 20:25:03 +0530

    Delete react-pokemon-develop directory

--- react-pokemon-develop/src/tests/redux/reducers/index.test.js
+++
@@ -1,8 +0,0 @@
-import pokemonReducer from '../../../redux/reducers/pokemon';
-
-describe('Pokemon Reducer', () => {
-  it('Should return default state', () => {
-    const newState = pokemonReducer(undefined, { loading: true, pokemon: {}, pokemons: [] });
-    expect(newState).toEqual({ loading: true, pokemon: {}, pokemons: [] });
-  });
-});
